ef2501e7cf0247a27ee259614854397d2df6375e
fmitei <faith.mitei@trustgro.com>
2023-02-16 06:00:43 +0300

    fixed search functionality

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleInputChange(e) {
+    console.log("....", e.target.value);
+    setInputText(e.target.value);
+  }
+
   return (
     <form className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={(e) => handleInputChange(e)} />
       </label>
 
       <label>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+    console.log("hi");
   }
 
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
+    if (selectedCategory === "All")
+      return item.name.toUpperCase().includes(searchValue.toUpperCase());
+    return (
+      item.category === selectedCategory &&
+      item.name.toUpperCase().includes(searchValue.toUpperCase())
+    );
   });
+
+  const onSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={onSearchChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
